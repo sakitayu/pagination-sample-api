45b48d3fdcdbded115480025351b8ee3715e5d3b
sakitayu <sakitakitakita@gmail.com>
2021-10-05 21:22:34 +0900

    不要なアクションなどを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,55 +2,11 @@
 
 class UsersController < ApplicationController
   include Pagy::Backend
-  before_action :set_user, only: %i[show update destroy]
 
-  # GET /users
   def index
     pagy, @users = pagy(User.all)
     response.headers['Total-Pages'] = pagy.pages
 
     render json: @users
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
-  end
-
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def user_params
-    params.require(:user).permit(:name)
   end
 end
