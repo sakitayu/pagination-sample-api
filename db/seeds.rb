babe89834bff2faf5b5027fbb9213c63948c746c
sakitayu <sakitakitakita@gmail.com>
2021-10-05 10:25:58 +0900

    100人分のユーザーのシードを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |n|
+  User.where(id: n + 1).first_or_create!(name: "test#{n + 1}")
+end
